77ceaac390ade10355b2a7f7b7e2279341725ede
jundymek <jundymek@gmail.com>
2019-11-04 00:20:37 +0100

    feat: booleans converts to yes no

--- src/js/main.js
+++ src/js/main.js
@@ -52,39 +52,47 @@ let users = [
   }
 ];
 
-const tableHead = document.querySelector(".table__head-tr--js");
-tableHead.addEventListener("click", e => {
-  console.log('saddsadsa')
-})
-for (const key of Object.keys(users[0])) {
-  if (key !== "id") {
-    tableHead.innerHTML += `<tr>
-  <th>${key}</th>
-  </tr>`;
+function createHeader(data) {
+  for (const key of Object.keys(data[0])) {
+    if (key !== "id") {
+      tableHead.innerHTML += `<tr>
+    <th>${key}</th>
+    </tr>`;
+    }
   }
 }
+
+const tableHead = document.querySelector(".table__head-tr--js");
+createHeader(users);
 
 const tableBody = document.querySelector(".table__body--js");
 
 for (let i = 0; i < users.length; i++) {
   const data = Object.entries(users[i]);
-  tableBody.innerHTML += `<tr key="${users[i]["id"]}" class="table__body-row table__body-row--${users[i]["id"]}"></tr>`;
-  for (const [key, value] of data) {
+  tableBody.innerHTML += `<tr id="${users[i]["id"]}" class="table__body-row table__body-row--${users[i]["id"]}"></tr>`;
+  for (let [key, value] of data) {
     const tableBodyRow = document.querySelector(`.table__body-row--${users[i]["id"]}`);
-    if (key !== 'id') {
+    if (key !== "id") {
+      if (typeof value === "boolean") {
+        if (value === true) {
+          value = "Yes"
+        } else {
+          value = "No"
+        }
+      }
       tableBodyRow.innerHTML += `<td>${value}</td>`;
-    }   
+    }
   }
 }
 
-const tableRow = document.querySelectorAll(".table__body-row")
+const tableRow = document.querySelectorAll(".table__body-row");
 for (const item of tableRow) {
   item.addEventListener("click", e => {
-    const node = e.target.parentNode
-    const cells = node.getElementsByTagName("td")
-    for (let i=0; i<cells.length; i++) {
-      console.log(cells[i].innerHTML)
+    const node = e.target.parentNode;
+    console.log(node.id);
+    const cells = node.getElementsByTagName("td");
+    for (let i = 0; i < cells.length; i++) {
+      console.log(cells[i].innerHTML);
     }
-  })
+  });
 }
-
