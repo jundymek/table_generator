2908413a934475854f5454a63ec7d1f23586b1c7
jundymek <jundymek@gmail.com>
2019-11-10 01:13:51 +0100

    feat: validateFormData helper function

--- src/js/createEditForm.js
+++ src/js/createEditForm.js
@@ -1,22 +1,26 @@
 import { form } from "./main";
 
+export let typeValidationData = []
 export function createEditForm(data) {
   const dataEntries = Object.entries(data[0]);
   for (let [key, value] of dataEntries) {
     if (key !== "id") {
       if (typeof value === "boolean") {
+        typeValidationData.push("checkbox")
         form.innerHTML += `<p class="form__element form__element--inactive form__element--js">
         <label class="form__label" for="checkbox">${key}</label>
         <input type="checkbox" name="${key}" id="${key}">
     </p>`;
       }
       if (typeof value === "string") {
+        typeValidationData.push("text")
         form.innerHTML += `<p class="form__element form__element--inactive form__element--js">
         <label class="form__label" for="${key}">${key}</label>
         <input required type="text" name="${key}" id="${key}" placeholder="">
     </p>`;
       }
       if (typeof value === "number") {
+        typeValidationData.push("number")
         form.innerHTML += `<p class="form__element form__element--inactive form__element--js">
         <label class="form__label" for="${key}">${key}</label>
         <input required type="number" name="${key}" id="${key}" step=0.5>
--- src/js/handleEdit.js
+++ src/js/handleEdit.js
@@ -1,25 +1,32 @@
 import { selectedID, form } from "./main";
 import { modifyTableRow } from "./modifyTableRow";
+import { typeValidationData } from "./createEditForm";
 
 export function handleEdit(e) {
   let newData = { id: selectedID };
-  let requiredFieldsFiled = true;
+  if (validateFormData(form)) {
+    for (let i = 0; i < form.length - 1; i++) {
+      if (form[i].type === "checkbox") {
+        form[i].checked ? (newData[`${form[i].id}`] = true) : (newData[`${form[i].id}`] = false);
+      } else {
+          newData[`${form[i].id}`] = form[i].value;
+        }
+      }
+      e.preventDefault()
+      const data = Object.entries(newData);
+      modifyTableRow(data);
+    }
+}
+
+function validateFormData(form) {
   for (let i = 0; i < form.length - 1; i++) {
-    if (form[i].type === "checkbox") {
-      form[i].checked ? (newData[`${form[i].id}`] = true) : (newData[`${form[i].id}`] = false);
-    } else {
-      if (form[i].value.length) {
-        console.log(form[i].type)
-        newData[`${form[i].id}`] = form[i].value;
-      }
-      else {
-        requiredFieldsFiled = false
-      }
+    if (form[i].value.length === 0) {
+        return false
+        }
+    if ((form[i].type !== typeValidationData[i])) {
+      form[i].setCustomValidity(`Expected ${typeValidationData[i]} value`);
+      return false
     }
   }
-  const data = Object.entries(newData);
-  if (requiredFieldsFiled) {
-    e.preventDefault()
-    modifyTableRow(data);
-  }
+  return true
 }
