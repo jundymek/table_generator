b82096664ace2a35b18ebf9dee0f4a3fbaa40602
jundymek <jundymek@gmail.com>
2019-11-11 23:31:45 +0100

    refactor: getHtmlElements() helper function

--- src/js/createBody.js
+++ src/js/createBody.js
@@ -1,17 +1,20 @@
 import { booleanConverts } from "./booleanConverts";
-import { tableBody } from "./main";
 
 export function createBody(data) {
   const tableBody = document.querySelector(".table__body--js");
   for (let i = 0; i < data.length; i++) {
     const dataEntries = Object.entries(data[i]);
-    tableBody.innerHTML += `<tr id="${data[i]["id"]}" class="table__body-row"></tr>`;
-    for (let [key, value] of dataEntries) {
-      const tableBodyRow = document.getElementById(`${data[i]["id"]}`);
-      if (key !== "id") {
-        value = booleanConverts(value);
-        tableBodyRow.innerHTML += `<td class="table__body-td">${value}</td>`;
-      }
-    }
+    tableBody.innerHTML += `<tr id="${data[i]["id"]}" class="table__body-row">${fillTableRow(dataEntries)}</tr>`;
+    ;
   }
 }
+
+function fillTableRow(dataEntries) {
+  const items = dataEntries.map((item) => {
+    if (item[0] !== "id") {
+    return `<td class="table__body-td">${booleanConverts(item[1])}</td>`
+    }
+  })
+  return items.join(" ")
+}
+
--- src/js/createHeader.js
+++ src/js/createHeader.js
@@ -1,7 +1,7 @@
-import { tableHead } from "./main";
+import { getHtmlElements } from "./main";
 
 export function createHeader(data) {
-  const tableHead = document.querySelector(".table__head-tr--js");
+  const tableHead = getHtmlElements().tableHead;
   for (const key of Object.keys(data[0])) {
     if (key !== "id") {
       tableHead.innerHTML += `<tr>
